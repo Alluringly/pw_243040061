52a8305af2eb40cb5e8b54a3ae344acc48c03b55
Alluringly <rizkianugerah64@gmail.com>
2025-02-14 03:45:44 -0800

    selesai mengerjakan tugas 1

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,13 @@
+<?php
+$nama = "Rizki A";
+$email = "rizkianugerah64@gmail.com";
+$pekerjaan = "-";
+$jurusan = "Teknik Informatika";
+$universitas = "Universitas Pasundan";
+
+echo "<h1>$nama</h1>";
+echo "<h4>$email</h4>";
+echo "<p>Halo, pekerjaan saya adalah $pekerjaan</p>";
+echo "<p>Jurusan saya $jurusan</p>";
+echo "<p>Saya kuliah di $universitas</p>";
+?>
